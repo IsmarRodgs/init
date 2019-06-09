dcc5bffef239ac9b50dc30f22df6c6bac05c67a8
Ismar Rodrigues <ismarrodrigues13@gmail.com>
2019-06-09 13:50:01 -0300

    Adicionando debug.

--- Teste.java
+++ Teste.java
@@ -1,5 +1,6 @@
 public class Teste{
     public static void main (String []args){
         System.out.print("Exemplo criado para demonstrar integração com o GIT");
+        System.out.print("Debug 1");
     }
 }
